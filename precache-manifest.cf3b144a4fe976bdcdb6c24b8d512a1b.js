d99e96b3b772c6474b36d674e1282593375bd9b7
Dahyun Kim <beska@naver.com>
2019-08-24 20:54:12 +0800

    Updates

---
+++ precache-manifest.cf3b144a4fe976bdcdb6c24b8d512a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ab14fd4eb9e698f084670dea2a2e37",
+    "url": "/material-matters/index.html"
+  },
+  {
+    "revision": "2abf5938b27ffbdc35b0",
+    "url": "/material-matters/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "abe10de98918eef906d7",
+    "url": "/material-matters/static/css/main.966d1a71.chunk.css"
+  },
+  {
+    "revision": "2abf5938b27ffbdc35b0",
+    "url": "/material-matters/static/js/2.21e0550d.chunk.js"
+  },
+  {
+    "revision": "abe10de98918eef906d7",
+    "url": "/material-matters/static/js/main.92ac7ce2.chunk.js"
+  },
+  {
+    "revision": "0c7a19fae65b7bde62f7",
+    "url": "/material-matters/static/js/runtime~main.f89498b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-matters/precache-manifest.f8b7b946b3269cf43f28c33f3e7535be.js"
+  "/material-matters/precache-manifest.cf3b144a4fe976bdcdb6c24b8d512a1b.js"
 );
 
 self.addEventListener('message', (event) => {
