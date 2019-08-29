b7103d17cfe5baf26f84a8027ac1309dc20c17d7
Dahyun Kim <beska@naver.com>
2019-08-30 00:55:56 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-matters/precache-manifest.b1955e8555f8f6ec3df06d7a70f7ceb6.js"
+  "/material-matters/precache-manifest.3da8ca25de35624d61d0add2ebacc754.js"
 );
 
 self.addEventListener('message', (event) => {
