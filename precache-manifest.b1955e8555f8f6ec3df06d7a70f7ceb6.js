650bfed7a6975c335f87a4bdeea2815e3897cb1a
Dahyun Kim <beska@naver.com>
2019-08-30 01:13:50 +0800

    Updates

--- precache-manifest.b1955e8555f8f6ec3df06d7a70f7ceb6.js
+++ precache-manifest.b1955e8555f8f6ec3df06d7a70f7ceb6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b43c755ba8dc91697bd9cf2fe3a3e3a5",
+    "revision": "69307a1070394ddc9c66a4695cfcc1e3",
     "url": "/material-matters/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/material-matters/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "d0c98199b56c24a0b3f0",
+    "revision": "12fe92e4576fe349a28a",
     "url": "/material-matters/static/css/main.512d83f9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/material-matters/static/js/2.59c061b9.chunk.js"
   },
   {
-    "revision": "d0c98199b56c24a0b3f0",
-    "url": "/material-matters/static/js/main.ce98e8f2.chunk.js"
+    "revision": "12fe92e4576fe349a28a",
+    "url": "/material-matters/static/js/main.4bae8905.chunk.js"
   },
   {
     "revision": "0c7a19fae65b7bde62f7",
